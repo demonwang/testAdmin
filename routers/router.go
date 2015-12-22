ec677406239cfcdcb849c80de65fcfe320aeedd8
Playboy王某某 <wm5821090@gmail.com>
2015-12-22 09:49:17 +0800

    OK

--- routers/router.go
+++ routers/router.go
@@ -3,7 +3,7 @@ package routers
 import (
 	"github.com/astaxie/beego" //beego 包
 	"github.com/beego/admin"   //admin 包
-	"testAdmin/controllers"        //自身业务包
+	"testAdmin/controllers"    //自身业务包
 )
 
 func init() {
